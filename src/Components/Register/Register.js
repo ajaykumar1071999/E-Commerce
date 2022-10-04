d1f108f178fc63481f33ab635a3ee735cafdede8
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-10-04 17:16:35 +0530

    print data in console

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,4 +1,47 @@
+import react from "react";
+import { useState } from "react";
 function Register() {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function GetData(e) {
+    console.log(name, email, password);
+  }
+  return (
+    <div className="col-sm-6 offset-sm-3">
+      <h1>User Registration Form</h1>
+
+      <input
+        type="text"
+        name="name"
+        className="form-control"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="text"
+        name="email"
+        className="form-control"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        type="password"
+        name="password"
+        className="form-control"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <br />
+      <button className="btn btn-primary" onClick={GetData}>
+        Submit
+      </button>
+    </div>
+  );
 }
 export default Register;
