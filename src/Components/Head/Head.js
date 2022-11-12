8812ae458c326cd2608ad333bbc2a18040119b7c
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-11-12 17:43:00 +0530

    Date Get

--- src/Components/Head/Head.js
+++ src/Components/Head/Head.js
@@ -1,6 +1,27 @@
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 function Head() {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  const date = new Date();
+  const month = months[date.getMonth()];
+  const showTime =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  const showDate =
+    date.getDate() + ":" + date.getMonth() + ":" + date.getFullYear();
+
   return (
     <div>
       <Navbar bg="dark" variant="dark">
@@ -10,6 +31,8 @@ function Head() {
 
           <Link to="/Register">Register</Link>
           <Link to="/Login">Login</Link>
+          <Link> Date :-{month}</Link>
+          <Link> Time :-{showTime}</Link>
         </Nav>
       </Navbar>
     </div>
