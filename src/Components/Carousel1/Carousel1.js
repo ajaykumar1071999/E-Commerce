3d395afe6666cba6d50d4c82b42e728e884d4594
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-11-22 18:25:36 +0530

    update

---
+++ src/Components/Carousel1/Carousel1.js
@@ -0,0 +1,22 @@
+function Carousel1() {
+  return (
+    <>
+      <div>
+        <div class="owl-carousel">
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+
+          <script src="jquery.min.js"></script>
+
+          <script src="owlcarousel/owl.carousel.min.js"></script>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Carousel1;
