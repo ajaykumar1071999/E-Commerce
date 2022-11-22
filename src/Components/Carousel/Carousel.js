b30831a76014c1a63c8072de966384238cceff6d
Ajay Kumar <99203990+ajaykumar1071999@users.noreply.github.com>
2022-11-22 17:22:36 +0530

    Update

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,17 +1,12 @@
 import "./Carousel.css";
 import backimg from "../Images/2.svg";
+import backimg1 from "../Images/1.png";
 function Carousel() {
   return (
     <>
       <section>
-        <div
-          className="section"
-          style={{
-            backgroundImage: `url(${backimg})`,
-            backgroundRepeat: "no-repeat",
-            position: "covered"
-          }}
-        ></div>
+        <div className="section"></div>
+
         <div
           className="section1"
           style={{
