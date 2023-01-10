7f246493733c46e9ece976dd9caac715d4c7b988
Ajay Kumar <ajaykumar1071999@gmail.com>
2023-01-10 17:02:27 +0530

    Changes

--- src/Components/BlogSection/BlogSection.js
+++ src/Components/BlogSection/BlogSection.js
@@ -1,24 +1,43 @@
-import './BlogSection.css';
-function BlogSection(){
-return(
+import "./BlogSection.css";
+function BlogSection() {
+  return (
     <>
-        <section className='blog-section'>
-            <div className='container'>
-            <div className='blog-section-heading'>
-                <div className='d-flex justify-content-between'>
-                    <div className='section-heading'>
-                    <h5 className='section-head-1'>Do you need childcare?</h5>
-                    </div>
-                    <div className='section-viewAll'>Browse More
-                    <i className='fa fa-arrow-circle-right'></i>
-                    </div>
-                </div>
-                <h2 className='section-head-2'>Speak to trusted Tllid carers</h2>
-                </div>
-
-                <div className='blog-section-card'></div>
+      <section className="blog-section">
+        <div className="container">
+          <div className="blog-section-heading">
+            <div className="d-flex justify-content-between">
+              <div className="section-heading">
+                <h5 className="section-head-1">Do you need childcare?</h5>
+              </div>
+              <div className="section-viewAll">
+                Browse More
+                <i className="fa fa-arrow-circle-right"></i>
+              </div>
             </div>
-        </section>
+            <h2 className="section-head-2">Speak to trusted Tllid carers</h2>
+          </div>
+
+          <div className="blog-section-card">
+            <div className="row">
+              <div className="col-md-3">
+                <div className="blog-section-card-1">
+                <div className="card-1-video">
+                    <video src="https://www.youtube.com/embed/-l-BjvF44Tc" controls="control"></video>
+                </div>
+                </div>
+              </div>
+
+              <div className="col-md-3">
+                <div className="blog-section-card-1"></div>
+              </div>
+              <div className="col-md-3">
+                <div className="blog-section-card-1"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-)
-}export default BlogSection;
+  );
+}
+export default BlogSection;
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,24 +3,24 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-      <div className="col-container">
-        <div className="col">
+    <div className="d-flex justify-content-between">
+    
+    <div className="col">
           <h2>About Us</h2>
           <p>Courses</p>
           <p>Templates</p>
           <p>Fee Structure</p>
         </div>
-        <div className="col">
+    <div className="col">
           <h2>Col-3</h2>
           <p>Hello</p>
           <p>Hello</p>
           <p>Hello</p>
         </div>
-        <div className="col">
+    <div className="col">
           <h2>Col-3</h2>
           <p>Hello</p>
           <p>Hello</p>
-
           <div className="font">
             <Link>
               <i className="fa-brands fa-twitter "></i>
