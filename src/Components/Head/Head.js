5a8d8342177d72a58eab78520570226cfad0ece0
Ajay Kumar <ajaykumar1071999@gmail.com>
2023-01-26 23:53:38 +0530

    done

--- src/Components/Head/Head.js
+++ src/Components/Head/Head.js
@@ -1,12 +1,39 @@
+import './Head1.css';
+import logo from '../Images/logo.png';
 function Head(){
   return(
 <>
   <section className="header-section">
-    <header>
-      <nav className="navigation">
-<div>Logo</div>
-<div>Menu</div>
-      </nav>
+    <header className='menu-header'>
+    <nav className='menu-nav'>
+      <div className='container'>
+        <div className='navigation-menu'>
+          <div className='navigation-logo'>
+            <img src={logo} alt="navigation logo"/>
+          </div>
+          <div className='navigation-item'>
+            <div>Home</div>
+            <div>Courses</div>
+            <div>Consultations</div>
+            <div>Activities</div>
+            <div>Events</div>
+            <div>Careers</div>
+          </div>
+          <div className='navigation-login'>
+            <div>
+            <i class="fas fa-headset"></i>
+            </div>
+            <div>
+            <i class="fas fa-user"></i>
+            
+            </div>
+            
+          </div>
+          <span className='nav-menu-bar'><i class="fa-solid fa-bars"></i></span>
+        </div>
+      </div>
+    </nav>
+    
     </header>
   </section>
 </>    
